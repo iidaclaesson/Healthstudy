f9f575d74726939986eb4657fdd9e305a9d17cd1
iidaclaesson <iidaclaesson@gmail.com>
2025-11-25 20:05:01 +0100

    stats in metrics

--- src/metrics.py
+++ src/metrics.py
@@ -1,2 +1,20 @@
 import pandas as pd
+import numpy as np
 
+def summary_stats(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Returns a DataFrame with mean, median, std, min, and max for the selected columns
+    """
+
+    stats = []
+    for col in ["age", "weight", "height", "systolic_bp", "cholesterol"]:
+        columns = {
+            "variable": col,
+            "mean": df[col].mean(),
+            "median": df[col].median(),
+            "std": df[col].std(),
+            "min": df[col].min(),
+            "max": df[col].max()
+        }
+        stats.append(columns)
+    return pd.DataFrame(stats)
