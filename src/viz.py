23b0c95085d9b4ed018c3c72646780b9fb4aff0c
iidaclaesson <iidaclaesson@gmail.com>
2025-11-25 22:47:36 +0100

    histogram to viz.py

--- src/viz.py
+++ src/viz.py
@@ -1,2 +1,14 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+
+
+
+def hist(ax, data, title, xlabel, ylabel, bins=30, edgecolor="black"):
+    ax.hist(data, bins=bins, edgecolor=edgecolor)
+    ax.set_title(title)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    return ax
+
+
+    
