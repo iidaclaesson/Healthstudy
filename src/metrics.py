8944d4c1c9a66a63e25d1532ce32f0d2a1c4917f
iidaclaesson <iidaclaesson@gmail.com>
2025-11-25 20:25:49 +0100

    import stats from metrics

--- src/metrics.py
+++ src/metrics.py
@@ -5,7 +5,6 @@ def summary_stats(df: pd.DataFrame) -> pd.DataFrame:
     """
     Returns a DataFrame with mean, median, std, min, and max for the selected columns
     """
-
     cols = ["age", "height", "weight", "systolic_bp", "cholesterol"]
 
     stats_df = df[cols].agg(["mean", "median", "min", "max"])
