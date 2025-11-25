a85a8bf603af2c923452d582ef1f0dc2df53eddf
iidaclaesson <iidaclaesson@gmail.com>
2025-11-25 23:04:46 +0100

    bar plot to viz.py

--- src/viz.py
+++ src/viz.py
@@ -11,4 +11,12 @@ def hist(ax, data, title, xlabel, ylabel, bins=30, edgecolor="black"):
     return ax
 
 
+def bar(ax, x, y, title, xlabel, ylabel, edgecolor="black", width=0.6):
+    ax.bar(x, y, edgecolor=edgecolor, width=width)
+    ax.set_title(title)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    return ax
+
+
     
